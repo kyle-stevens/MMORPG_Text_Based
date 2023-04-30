02a18076db6d71bc98ec17ba5e43d6a5f334269f
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2023-04-30 14:25:50 -0700

    Building Login Page

--- LoginPage/login_page.gd
+++ LoginPage/login_page.gd
@@ -1,9 +1,9 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = str(get_viewport().size)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
